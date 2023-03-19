3cef7735e053806226fd81cd993a415b2cfa1448
4b1g0011 <4b1g0011@office.stust.edu.tw>
2023-03-19 20:44:25 +0800

    加入專案檔案。

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+using namespace
+std;
+int main)
+f
+int a;
+for (int i = 0;; 1++）
+	f
+	cin >> a:
+if (a <= 0| |a > 12)
+cout << "错误，
+请重新输入"<<endl;
+else
+if (a >= 3 & 8a = 5）
+	cout << "春季" << endl;
+	if (a >= 6 && a <= 8)
+		coutx < "夏季" << endl;
+	if (a >= 9 && a <= 11)
+		coutx < "秋季" << endl;
+	if (a <= 2 && a > 0 a == 12)
+		cout* < "冬季" << endl;
+}
+system("pause");
+return 0;
+}
